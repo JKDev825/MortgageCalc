c927d6a8960e6ee2a5a045f11f45db7532d4c075
JKDev825 <79871179+JKDev825@users.noreply.github.com>
2021-04-19 02:40:41 -0400

    save point: tried to add sweetalert

--- js/site.js
+++ js/site.js
@@ -97,7 +97,15 @@
          resultsBody.appendChild(dataRow);
      }
 
-     displayTermTotals()
+     /*
+      ** .Running totals are calculated on iterations.  This impacts monthly values are rates are recalculated.
+      ** .Update the global variables for total cost displays.
+      ** .displayTermTotals() references the global totals.
+      */
+
+     g_termIntTotal = tbl_totIntPaid;
+
+     displayTermTotals();
      return null;
  } /* end of displayPaymentPlan() */
 
@@ -107,110 +115,16 @@
   */
  function displayTermTotals() {
 
+     document.getElementById("resultMonPayment").innerHTML = numtoMoneyStrWDS(g_totMonPayment);
      document.getElementById("resultTotalPrincipal").innerHTML = numtoMoneyStrWDS(g_termPayAmt);
      document.getElementById("resultTotalInterest").innerHTML = numtoMoneyStrWDS(g_termIntTotal);
      document.getElementById("resultTotalCost").innerHTML = numtoMoneyStrWDS(g_termPayAmt + g_termIntTotal);
 
 
      return null;
- }
+ } /* end of displayTermTotals() */
 
 
- /**
-  ** test code 
-  **
-  */
- /* testPaymentCalc(); */
-
-
- /************************************
-
- function loadEventData() {
-     let eventData = [];
-     eventData = getEventData();
-     displayEventData(eventData);
-
-     return null;
- } // end of loadEventData()
-
- function saveEventFormData() {
-
-     // grab the events out of local storage
-     let eventData = JSON.parse(localStorage.getItem("eventArray")) || eventArray;
-
-     let obj = {};
-
-     obj["event"] = document.getElementById("newEvent").value;
-     obj["city"] = document.getElementById("newCity").value;
-     obj["state"] = document.getElementById("newState").value;
-     obj["attendance"] = document.getElementById("newAttendance").value;
-     obj["date"] = document.getElementById("newDate").value;
-
-     eventData.push(obj);
-
-     localStorage.setItem("eventArray", JSON.stringify(eventData));
-
-     return null;
- } // end of saveEventData()
-
- function displayEventData(eventData) {
-     const template = document.getElementById("Event-Data-Template");
-     const resultsBody = document.getElementById("resultsBody");
-
-     resultsBody.innerHTML = "";
-
-     for (let i = 0; i < eventData.length; i++) {
-         const dataRow = document.importNode(template.content, true);
-
-         dataRow.getElementById("eventname").textContent = eventData[i].event;
-         dataRow.getElementById("city").textContent = eventData[i].city;
-         dataRow.getElementById("state").textContent = eventData[i].state;
-         dataRow.getElementById("attendance").textContent = eventData[i].attendance;
-         dataRow.getElementById("date").textContent = formatDateMMDDYYYY(eventData[i].date);
-
-         resultsBody.appendChild(dataRow);
-     }
-     return null;
- } // end of displayEvenData()
-
- ****************************************************************/
-
-
- function testPaymentCalc() {
-
-     /*
-      **
-      ** From Bobby Davis to Everyone: 05: 44 PM in chat
-      ** rate = rate / 1200;
-      **  Converts to a monthly rate
-      */
-     let termPayAmt = 0; /* total loan amount: user input */
-     let termPayYrs = 0; /* total years: this will be user input */
-     let termIntRate = 0; /* term interest rate: user input */
-     let totInterestAmt = 0; /* total dollar amt for full term */
-     let termPayMos = 0; /* total number of months: yrs * 12 */
-
-     let totMoPrincAmt = 0; /* monthly loan amt */
-     let totMoInterest = 0; /* monthly interest amt */
-
-
-     // initialize variables
-     termPayMos = totPayYrs * 12; /* get the total months for the term */
-
-     // set up test values
-     termPayAmt = 10000.00; /* total loan amount */
-     termPayYrs = 5; /* will be from user input */
-     termIntRate = 5; /* term interest rate as a decimal; not a true percent */
-     totMoInterest = termIntamt / termPayMos; /* monthly interest payment is total interest divided total months */
-
-
-     totMoPrincAmt = totPayAmt / (totPayYrs / 12);
-     totInterestAmt = calcPercentage(totPayAmt, 5); /* total interest dollar amt for the full term */
-
-     console.log(`totamt:${totPayAmt} totmos:${totPayMos} totPer:${totPerAmt}`);
-     alert("in test payment calc")
-
- }
 
  function calcPercentage(num, percent) {
 
@@ -236,7 +150,12 @@
          minimumFractionDigits: 2
      });
 
+     /*
+      ** .negative values will report "-US$0.00".  toLocalString changes negatives to 0.00
+      ** .until additional work is applied also remove the negative sign.
+      */
      returnMoneyStr = tmpStr.replace("US$", "");
+     returnMoneyStr = returnMoneyStr.replace("-", "");
      return returnMoneyStr;
  }
  /**
